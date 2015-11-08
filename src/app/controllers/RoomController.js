e4a99adc3ff8aeccf352b037644132849bc99e11
Ian Williams <ianwilliams633@gmail.com>
2015-11-08 15:19:39 -0800

    added src to git ignore

--- src/app/controllers/RoomController.js
+++
@@ -1,8 +0,0 @@
-import BaseController from './BaseController';
-
-export default class extends BaseController {
-    constructor(players) {
-        //this.players = players;
-        //this.room = 
-    }
-}
