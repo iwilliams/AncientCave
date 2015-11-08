a454323961e00a17e47dd3c72daeee43e35c980f
Ian Williams <ianwilliams633@gmail.com>
2015-11-08 15:15:04 -0800

    Got rid of bower components

--- bower_components/Keypress/meteor/export.js
+++
@@ -1,3 +0,0 @@
-/*global Keypress:true*/  // Meteor.js creates a file-scope global for exporting. This comment prevents a potential JSHint warning.
-Keypress = window.keypress;
-delete window.Keypress;
